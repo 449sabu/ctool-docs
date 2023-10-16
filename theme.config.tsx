8eeb1ad952c8c62cb27c93eb5da0fb382f21801a
449sabu <449sabu@gmail.com>
2023-10-16 13:49:14 +0900

    docs: new contants

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
-export default {
-  logo: <span>CIEL DOCS</span>,
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig ={
+  logo: <strong>CIEL CARDANO GUIDEBOOK</strong>,
   project: {
     link: 'https://github.com/449sabu',
   },
@@ -8,5 +10,8 @@ export default {
   i18n: [
     { locale: 'ja', text: '日本語'},
     // { locale: 'en', text: 'English' }
-  ]
+  ],
+  docsRepositoryBase: "https://github.com/449sabu/ctool-docs/tree/main",
 }
+
+export default config;
